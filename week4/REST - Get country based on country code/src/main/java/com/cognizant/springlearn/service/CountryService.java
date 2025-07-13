28247b064ee4d7fd170260a4987472f73eb3b1d5
jo-27 <joansharon270704@gmail.com>
2025-07-13 10:43:50 +0530

    week4 updated successfully

---
+++ week4/REST - Get country based on country code/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,25 @@
+package com.cognizant.springlearn.service;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.springlearn.model.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+        @SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        @SuppressWarnings("unchecked")
+		List<Country> countries = (List<Country>) context.getBean("countryList");
+
+        return countries.stream()
+                .filter(c -> c.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null); // Or throw exception if not found
+    }
+}
