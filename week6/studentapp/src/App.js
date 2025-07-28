c2c65e43c6383a7bafa14f4a27c667d92a72f316
jo-27 <joansharon270704@gmail.com>
2025-07-28 11:00:45 +0530

    week6 updated

---
+++ week6/studentapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Components/HomePage"
+import About from "./Components/AboutPage"
+import Contact from "./Components/ContactPage"
+
+function App() {
+  return (
+    <div>
+    <Home/>
+    <About/>
+    <Contact/>
+    </div>
+  );
+}
+
+export default App;
